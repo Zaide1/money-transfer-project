114c48100f0b36c270b95d683189eb9efa228c95
Zaide1 <zaideebrahim@gmail.com>
2023-03-17 23:11:19 +0000

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Money Transfer
+          money Transfer
         </h1>
 
         <p className={styles.description}>
