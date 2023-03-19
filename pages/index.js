3ed54fb4420dbf125fd0113900c696454fbbd7b2
Zaide1 <69059158+Zaide1@users.noreply.github.com>
2023-03-19 01:20:38 +0000

    new file:   images/finance-1.jpg 	new file:   images/tech-main.jpg 	modified:   pages/index.js 	modified:   styles/Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
@@ -6,11 +7,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Srai Shazada Money Transfer ZAHID- Secure and Fast Online Transactions</title>
+        <title>Srai Shazada Money Transfer - Secure and Fast Online Transactions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.textWrapper}>
         <h1 className={styles.title}>
           Money Transfer
         </h1>
@@ -18,28 +20,31 @@ export default function Home() {
         <p className={styles.description}>
           Secure and fast online transactions
         </p>
+       </div>
 
         <div className={styles.grid}>
-          <Link href="/send-money" className={styles.card}>
-            <h3>Send Money &rarr;</h3>
-            <p>Send money to anyone, anywhere in the world.</p>
-          </Link>
+        <Link href="/send-money" className={`${styles.card} card`}>
+  <h3>Send Money &rarr;</h3>
+  <p>Send money to anyone, anywhere in the world.</p>
+</Link>
 
-          <Link href="/receive-money" className={styles.card}>
-            <h3>Receive Money &rarr;</h3>
-            <p>Receive money from anyone, anywhere in the world.</p>
-          </Link>
+<Link href="/receive-money" className={`${styles.card} card`}>
+  <h3>Receive Money &rarr;</h3>
+  <p>Receive money from anyone, anywhere in the world.</p>
+</Link>
 
-          <Link href="/track-transfer" className={styles.card}>
-            <h3>Track Transfer &rarr;</h3>
-            <p>Track the status of your money transfer.</p>
-          </Link>
+<Link href="/track-transfer" className={`${styles.card} card`}>
+  <h3>Track Transfer &rarr;</h3>
+  <p>Track the status of your money transfer.</p>
+</Link>
 
-          <Link href="/rates" className={styles.card}>
-            <h3>Exchange Rates &rarr;</h3>
-            <p>View our competitive exchange rates.</p>
-          </Link>
+<Link href="/rates" className={`${styles.card} card`}>
+  <h3>Exchange Rates &rarr;</h3>
+  <p>View our competitive exchange rates.</p>
+</Link>
+
         </div>
+      
       </main>
 
       <footer className={styles.footer}>
